95a5ad1db587cee895f0ddf775a52b488accf704
Ellie Young <43616100+elllie@users.noreply.github.com>
2021-04-02 22:25:10 -0400

    Create 2-variables.py

---
+++ chapters/1-PythonBasics/2-variables.py
@@ -0,0 +1,19 @@
+---
+title: "2. A Bite of Data"
+parent: "1. Python Basics"
+has_children: false
+has_toc: false
+nav_order: 2
+---
+
+# Byte 1-2: A Bite of Data
+
+This byte is all about getting computers to save and remember information using variables. We chat with an expert about what variables are, why they’re useful, how they work, and how to use them.
+
+(iplayer here)
+
+## This byte's guest: [Caitrin Eaton](https://www.ncf.edu/directory/listing/caitrin-eaton/)
+
+![Photo of Professor Eaton](https://photos.smugmug.com/photos/i-S52gsXG/0/2e0cd0ee/S/i-S52gsXG-S.jpg)
+
+Professor Eaton designs energy-efficient, biologically informed robots and teaches courses in embedded systems and computer architecture. She is masterfully skilled at optimizing hardware to store a lot of thoughts in not a lot of space. 
