06f967d9a03c033b88eb2f1a9d4d0fd49b5769f2
Ellie Young <43616100+elllie@users.noreply.github.com>
2021-04-02 22:27:48 -0400

    Create 3-PracticingPythonBasics.py

---
+++ chapters/1-PythonBasics/3-PracticingPythonBasics.py
@@ -0,0 +1,15 @@
+---
+title: "3. Practicing Python Basics"
+parent: "1. Python Basics"
+has_children: false
+has_toc: false
+nav_order: 3
+---
+
+# Byte 1-3: Practicing Python Basics
+
+stuff
+
+(iplayer here)
+
+# sdfghjkl
