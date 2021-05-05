8d8102c688b7ae1f8a152413fb53b5985fa65868
mlepinski <mlepinski@gmail.com>
2021-05-05 09:28:28 -0400

    Create 2-QandA.py

---
+++ chapters/2-CSSecrets/2-QandA.py
@@ -0,0 +1,14 @@
+---
+title: "2. Listener Question about AI"
+parent: "2. You & AI"
+has_children: false
+has_toc: false
+nav_order: 2
+---
+
+# 2-2: Listener Question about AI 
+
+Artificial Intelligence is a phrase that is frequently tossed around but is often misunderstood. We answer an excellent listern question and discuss what AI really is. 
+
+(iframe)
+
